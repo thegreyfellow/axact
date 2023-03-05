2e2441aa3025608bc011cd869ad2a8c83b92bfff
blank <youssef.idmoussi@gmail.com>
2023-03-05 16:37:59 +0100

    Add sysinfo, return CPUs state in GET / .

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use axum::extract::State;
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use axum::{routing::get, Router, Server};
+use sysinfo::{CpuExt, System, SystemExt};
+
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
+    let state = AppState {
+        sys: Arc::new(Mutex::new(System::new())),
+    };
+    let router = Router::new().route("/", get(root_get)).with_state(state);
     let server = Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on http://{}", addr);
@@ -10,7 +23,17 @@ async fn main() {
     server.await.expect("Failed to start server");
 }
 
-async fn root_get() -> &'static str {
-    println!("Hello, I'm root!");
-    "Hello, I'm root!"
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut cpus = String::new();
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+        writeln!(cpus, "CPU {}: {}%", i, cpu.cpu_usage()).unwrap();
+    }
+
+    cpus
 }
